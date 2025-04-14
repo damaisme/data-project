52622627cf36e2f7c157ac40cc01e5f5f32a876f
root <root@labms1.boer.id>
2025-04-14 15:57:35 +0700

    update cloud init

--- tf-k8s-cluster/main.tf
+++ tf-k8s-cluster/main.tf
@@ -22,14 +22,17 @@ resource "libvirt_volume" "k8s_disk" {
   count  = length(var.nodes)
   name   = "${var.nodes[count.index].name}.qcow2"
   pool   = "vms"
-  source = "/data/isos/ubuntu-jammy.img"
+  base_volume_name = "ubuntu-jammy.img"
+  base_volume_pool = "isos"
   format = "qcow2"
+  size   = "107374182400"
 }
 
 resource "libvirt_cloudinit_disk" "cloudinit" {
   count     = length(var.nodes)
   name      = "${var.nodes[count.index].name}-init.iso"
-  user_data = file("${path.module}/cloud-init/node.yaml")
+  user_data = file("${path.module}/cloud-init/cloud-${var.nodes[count.index].name}.yaml")
+  network_config = file("${path.module}/cloud-init/net-${var.nodes[count.index].name}.yaml")
 }
 
 resource "libvirt_domain" "k8s_node" {
@@ -37,15 +40,39 @@ resource "libvirt_domain" "k8s_node" {
   name   = var.nodes[count.index].name
   memory = 8192
   vcpu   = 4
+  machine = "pc"
+
 
   disk {
     volume_id = libvirt_volume.k8s_disk[count.index].id
   }
 
+  console {
+    type        = "pty"
+    target_port = "0"
+    target_type = "serial"
+  }
+
+  console {
+    type        = "pty"
+    target_port = "1"
+    target_type = "virtio"
+  }  
+
   network_interface {
     network_name = "dama-data-net"
     addresses    = [var.nodes[count.index].ip]
   }
+
+  video {
+    type = "vga"
+  }
+
+  graphics {
+    type = "vnc"
+    listen_type = "address"
+    autoport = true
+ }
 
   cloudinit = libvirt_cloudinit_disk.cloudinit[count.index].id
 }
